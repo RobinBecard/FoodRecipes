e743337112127aa76e22ece74a0c44d50698b0a0
robinbecard <robinbecard@gmail.com>
2025-02-13 09:36:15 +0100

    Login implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 @Component({
-  selector: 'app-login',
+  selector: 'login-form',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
 
+  private auth = inject(Auth);
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  @Input() error: string | null | undefined;
+  @Output() submitEM = new EventEmitter();
+
+  submit() {
+    if (this.form.valid) {
+      this.logIn();
+    }
+  }
+
+  logIn() {
+    const { email, password } = this.form.value; // Récupération des valeurs du formulaire
+
+    if (email && password) {
+      signInWithEmailAndPassword(this.auth, email, password)
+        .then((userCredential) => {
+          console.log('Connexion réussie :', userCredential.user);
+        })
+        .catch((error) => {
+          console.error('Erreur de connexion :', error.code, error.message);
+        });
+    }
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 const MaterialComponents = [
   MatButtonModule,
   MatSidenavModule,
+  MatFormFieldModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule
+  
 ];
 
 @NgModule({
@@ -15,4 +23,5 @@ const MaterialComponents = [
     MaterialComponents
   ]
 })
+
 export class MaterialModule { }
