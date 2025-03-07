ffedf0ce57e1ebbebfb3e70f0665f076e61ae387
Paul Badeuille <paul.badeuille@aum.bio>
2025-03-07 10:25:47 +0100

    update ng v19

--- src/app/components/list-sidenav/list-sidenav.component.ts
+++ src/app/components/list-sidenav/list-sidenav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list-sidenav',
-  templateUrl: './list-sidenav.component.html',
-  styleUrl: './list-sidenav.component.css'
+    selector: 'app-list-sidenav',
+    templateUrl: './list-sidenav.component.html',
+    styleUrl: './list-sidenav.component.css',
+    standalone: false
 })
 export class ListSidenavComponent {
 
--- src/app/components/test-info-log/test-info-log.component.ts
+++ src/app/components/test-info-log/test-info-log.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-test-info-log',
-  templateUrl: './test-info-log.component.html',
-  styleUrl: './test-info-log.component.css'
+    selector: 'app-test-info-log',
+    templateUrl: './test-info-log.component.html',
+    styleUrl: './test-info-log.component.css',
+    standalone: false
 })
 export class TestInfoLogComponent {
 
