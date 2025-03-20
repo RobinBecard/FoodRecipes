c4b3c3fa740f77deadc51f52ea748739cc7bf1cb
Cd-han <cedrichan13@gmail.com>
2025-03-20 09:41:04 +0100

    recette description

--- src/app/components/description/description.ts
+++ src/app/components/description/description.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Meal } from '../../models/meal.model';
 import { ApiService } from '../../service/api.service';
 
@@ -8,16 +8,6 @@ import { ApiService } from '../../service/api.service';
   styleUrls: ['./description.css'], // Correction ici
   standalone: false,
 })
-export class DescriptionComponent implements OnInit {
-  
-  Recipe!: Meal; // Ajout de la propriété meal
-
-  constructor(private mealService: ApiService) {} // Correction du constructeur
-
-  ngOnInit(): void {
-    this.mealService.getMealById('52771').subscribe((meal) => {
-      this.Recipe = meal;
-      console.log(meal)
-    });
-  }
+export class DescriptionComponent{
+  @Input() Recipe!: Meal;
 }
