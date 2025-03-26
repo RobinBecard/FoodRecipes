19bd8349fdedf1e714ab5241604aa99356b71d45
matthysp <mpezieredevoir@gmail.com>
2025-03-26 19:06:44 +0100

    profil modif MDP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -92,7 +92,7 @@ import { VideoplayerComponent } from './videoplayer/videoplayer.component';
         projectId: 'foodrecipes-2aa22',
         appId: '1:884813621746:web:33a2d8b48d913f4dc3636b',
         storageBucket: 'foodrecipes-2aa22.firebasestorage.app',
-        apiKey: 'AIzaSyCX8mcEGxhnFox8kfUBQsE3tTzJKdExp6c',
+        apiKey: 'AIzaSyA9gcG436HXnGSzICgferuINJeaAsa5bfg',
         authDomain: 'foodrecipes-2aa22.firebaseapp.com',
         messagingSenderId: '884813621746',
       })
